cdf246f87fecf811a65a94313bc1c862729d0ed1
Ouhfi <ouhfid4@gmail.com>
2026-05-21 10:29:41 +0100

    add logo ana text

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from "react-native";
-import ButtonComponent from "./component/buton";
+import { Text, View , Image } from "react-native";
+import  ButtonComponent  from "./component/Button";
+import Home from "./component/Home";  
+
+
 export default function App() {
   return (
     <View>
-      
+     <Home/>
       <Text>
         Hello, World!
       </Text>
